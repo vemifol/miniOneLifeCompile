ab475512a03043ea2ae27b255930cc7445f77d56
ForsWall <70001673+vemifol@users.noreply.github.com>
2026-04-13 10:36:11 -0700

    update repo links to mine

--- cloneRepos.sh
+++ cloneRepos.sh
@@ -2,9 +2,9 @@
 set -e
 cd "$(dirname "${0}")/.."
 
-CODE_REPO="https://github.com/twohoursonelife/OneLife.git"
-DATA_REPO="https://github.com/twohoursonelife/OneLifeData7.git"
-GEMS_REPO="https://github.com/twohoursonelife/minorGems.git"
+CODE_REPO="https://github.com/vemifol/OneLife.git"
+DATA_REPO="https://github.com/vemifol/OneLifeData7.git"
+GEMS_REPO="https://github.com/vemifol/minorGems.git"
 
 if [[ ! -d OneLife ]]; then git clone $CODE_REPO OneLife; fi
 if [[ ! -d OneLifeData7 ]]; then git clone $DATA_REPO OneLifeData7; fi
